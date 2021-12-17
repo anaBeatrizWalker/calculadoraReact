a9973bcc0a05a905ab63fb93c71a743f4d53a2b0
Ana Beatriz <anabiawalker@gmail.com>
2021-12-17 17:43:13 -0300

    Adicionando Funcionalidades nos Botões

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,5 +2,33 @@ import React from "react";
 import './Button.css'
 
 export default props => {
-    <button className="button">{props.label}</button>
+    <button className={`
+        button
+        ${props.operation ? 'operation' : ''}
+        ${props.double ? 'double' : ''}
+        ${props.triple ? 'triple' : ''}
+    `}>
+
+        {props.label}
+    </button>
+
+    /* OU */
+
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+    return (
+        <button 
+            className={classes}
+            onClick={e =>props.click && props.click(props.label)}>
+            {props.label}
+        </button>
+    )
+    
 }
+/*
+props.operation ? 'operation' : '' = se a propriedade operation for passada, use a classe
+
+props.click(props.label)}: pega o conteúdo do botão clicado
+*/
