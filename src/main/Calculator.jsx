05a891ff5ddd9fde282de865ee6bdd097eec483f
Ana Beatriz <anabiawalker@gmail.com>
2021-12-17 18:12:45 -0300

    Adicionando Funções nos Botões

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -3,7 +3,17 @@ import Button from '../components/Button'
 import Display from '../components/Display'
 import './Calculator.css'
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null, //armazena a operação
+    values: [0, 0], //array com valor 1 e valor 2
+    current: 0 //valor atual, indice 0 ou 1
+}
+
 export default class Calculator extends Component{
+
+    state = {...initialState} //clone do objeto relacionado a state
 
     constructor(props){
         super()
@@ -13,7 +23,7 @@ export default class Calculator extends Component{
     }
 
     clearMemory(){
-
+        this.setState({...initialState})
     }
 
     setOperation(operation){
@@ -21,13 +31,35 @@ export default class Calculator extends Component{
     }
 
     addDigit(n){
+        //Se foi digitado um ponto e já tinha um ponto incluso no display
+        if(n === '.' && this.state.displayValue.includes('.')){
+            return //ignora
+        }
+        
+        //Limpar o display quando o número for 0 ou quando clearDisplay for true
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
 
+        //Valor atual
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+
+        //Quando um número for colocado no display ele é o valor atual
+        const displayValue = currentValue + n
+
+        this.setState({displayValue, clearDisplay:false})
+
+        if(n !== '.'){
+            const i = this.state.current //pega o indice do valor (0)
+            const newValue = parseFloat(displayValue)//converte
+            const values = [...this.state.values] //pega o valor
+            values[i] = newValue //coloca o valor no indice atual
+            this.setState({values})
+        }
     }
 
     render(){
         return (
             <div className='calculator'>
-                <Display value={100} />
+                <Display value={this.state.displayValue} />
                 <Button label="AC" click={this.clearMemory} triple/>
                 <Button label="/" click={this.setOperation} operation/>
                 <Button label="7" click={this.addDigit}/>
